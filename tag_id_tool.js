aaa959db2059c56ef8f766eb174c03acb7c2de31
fourk <40277640+FourCinnamon0@users.noreply.github.com>
2022-02-02 17:15:33 +0000

    Refactor

--- tag_id_tool.js
+++ tag_id_tool.js
@@ -1,76 +1,79 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
-});
-function id2tag(id){
+function id2tag(id) {
 	id = id * 1
 	var tagChar = ["0", "2", "8", "9", "P", "Y", "L", "Q", "G", "R", "J", "C", "U", "V"]
 
 	var hashtag = []
-	
+
 	while (id > 0) {
 		hashtag.unshift(tagChar[id % tagChar.length])
 		id = Math.floor(id / tagChar.length)
 	}
-	
+
 	return "#" + hashtag.join("")
 }
-function tag2id(hashtag){
+
+function tag2id(hashtag) {
 	var tagChar = ["0", "2", "8", "9", "P", "Y", "L", "Q", "G", "R", "J", "C", "U", "V"]
 
 
 	var tagArray = hashtag.toUpperCase().split("")
-	if(tagArray[0] == "#"){
-	    tagArray.shift()
+	if (tagArray[0] == "#") {
+		tagArray.shift()
 	}
 	var id = 0
-	for(var i = 0;i < tagArray.length;i++){
+	for (var i = 0; i < tagArray.length; i++) {
 		var character = tagArray[i]
 		var charIndex = tagChar.indexOf(character)
-		if(charIndex == -1){console.error(`Invalid Tag! Tags can only contain the following characters: ${tagChar.join(", ")}`);process.exit(1)}
-    	id *= tagChar.length
-	    id += charIndex
+		if (charIndex == -1) {
+			console.error(`Invalid Tag! Tags can only contain the following characters: ${tagChar.join(", ")}`);
+			process.exit(1)
+		}
+		id *= tagChar.length
+		id += charIndex
 	}
 	return id
 }
 
 
 function hl2id(hl) {
-	if(typeof hl  == "string"){
+	if (typeof hl == "string") {
 		hl2idl = hl.split("-")
 	}
 	var id = 0;
 	id += parseInt(hl[0])
-	id += Math.floor(hl[1] * 256)// << 8
+	id += Math.floor(hl[1] * 256) // << 8
 
 	return id;
 }
-function id2hl(hashtag){
+
+function id2hl(hashtag) {
 	var id;
-    if(typeof hashtag == "string"){
-        id = tag2id(hashtag)
-    }else{
-        id = hashtag
-    }
-    var highLow = []
-
+	if (typeof hashtag == "string") {
+		id = tag2id(hashtag)
+	} else {
+		id = hashtag
+	}
+	var highLow = []
 	highLow[0] = (id % 256)
-
 	highLow[1] = ((id - highLow[0]) >> 8)
-
 
 	return highLow
 }
+
 // everything below this is a wrapper for the above functions
 // if you want to use this with your own code there's no need for anything below this line
 // also READ THE LICENSE
 
-var isInt = false; 	// `INT` stands for `INTeractive`, obviously
-if(process.argv[2]){
-	doThings(process.argv[2],process.argv[3])
-}else{
+console.log("")
+const readline = require('readline');
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+var isInt = false; // `INT` stands for `INTeractive`, obviously
+if (process.argv[2]) {
+	doThings(process.argv[2], process.argv[3])
+} else {
 	process.stdout.write("Syntax: <type> <input>\n> ")
 	isInt = true
 	rl.on('line', (input) => {
@@ -78,41 +81,54 @@ if(process.argv[2]){
 		process.stdout.write("> ")
 	});
 }
-function doThings(type,input) {
+
+function doThings(type, input) {
 	// Uncomment for timer
 	// var s = new Date().getTime();
 	switch (type) {
-		case "tag2id":case "tag2hl":case "tag":
-			if(!input){
+		case "tag":case "tag2id":case "tag2hl":
+			if (!input) {
 				console.error("No tag specified!")
-				if(!isInt){process.exit()}else{return;}
+				if (!isInt) {
+					process.exit()
+				} else {
+					return;
+				}
 			}
 			var gid = tag2id(input)
-			console.log("ID:    "+gid)
-			console.log("HL ID: "+id2hl(gid).join("-"))
+			console.log("ID:    " + gid)
+			console.log("HL ID: " + id2hl(gid).join("-"))
 			break;
-		case "hl2id":case "hl2tag":case "hl":
-			if(!input){
+		case "hl":case "hl2id":case "hl2tag":
+			if (!input) {
 				console.error("No HL ID specified!")
-				if(!isInt){process.exit()}else{return;}
+				if (!isInt) {
+					process.exit()
+				} else {
+					return;
+				}
 			}
 			var gid = hl2id(input.split("-"))
-			console.log("Tag:    "+id2tag(gid))
-			console.log("ID:     "+gid)
+			console.log("Tag:    " + id2tag(gid))
+			console.log("ID:     " + gid)
 			break;
-		case "id2hl":case "id2tag":case "id":
-			if(!input){
+		case "id":case "id2hl":case "id2tag":
+			if (!input) {
 				console.error("No HL ID specified!")
-				if(!isInt){process.exit()}else{return;}
+				if (!isInt) {
+					process.exit()
+				} else {
+					return;
+				}
 			}
-			console.log("Tag:    "+id2tag(parseInt(input)))
-			console.log("HL ID:  "+id2hl(parseInt(input)).join("-"))
+			console.log("Tag:    " + id2tag(parseInt(input)))
+			console.log("HL ID:  " + id2hl(parseInt(input)).join("-"))
 			break;
 		case "help":
 			console.log("Usage: `node tag_id_tool <type> <tag/id>`\nValid input types: hl/id/tag\nExample: `node tag_id_tool tag #2pp`\nTag-ID tool also supports stdin")
 			return;
 		case "exit":
-			process.exit()
+			process.exit();
 			return;
 		case "":
 			return;
